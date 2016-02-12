ead1b47cceb16f18b50256408355da55f5341983
joey <joey.novak@gmail.com>
2016-02-11 23:56:29 -0700

    Initial Commit

---
+++ src/Record.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: joey
+ * Date: 2/11/2016
+ * Time: 11:35 PM
+ */
+
+class Record {
+
+} 
---
+++ test/DocumentationParserTest.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: joey
+ * Date: 2/11/2016
+ * Time: 11:43 PM
+ */
+
+class DocumentationParserTest extends PHPUnit_Framework_TestCase{
+    public function testGetDocumentation(){
+        $this->assertEquals(0, 0);
+    }
+} 
